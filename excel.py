970afd144467f6f5e28346f088c7f76b608df41c
Micol Panetta <mi.panetta@reply.it>
2020-11-01 18:15:20 +0100

    fix borders

--- excel.py
+++ excel.py
@@ -2,6 +2,19 @@ from openpyxl import Workbook
 from openpyxl.styles import Font, Border, Side, PatternFill, Font, GradientFill, Alignment
 from datetime import datetime
 
+def set_borders_style(sheet, range1, range2):
+    side = Side(border_style="medium")
+    border = Border(
+        left = side,
+        right = side,
+        top = side,
+        bottom  = side,
+    )
+    cell_range = sheet[range1 : range2]
+    for row in cell_range:
+        for cell in row:
+            cell.border = border
+
 def save_excel(b2b, see, ids):
     wb = Workbook()
     b2b_sheet = wb.active
@@ -9,6 +22,9 @@ def save_excel(b2b, see, ids):
     see_sheet = wb.create_sheet(title="SEE")
     result_sheet = wb.create_sheet(title="Result")
     result_sheet.append(["Key", "Number of KO", "Result Counter"])
+    result_sheet.column_dimensions['A'].width = 11
+    result_sheet.column_dimensions['B'].width = 15
+    result_sheet.column_dimensions['C'].width = 15
 
     cell_range = result_sheet['A1':'C1']
     for row in cell_range:
@@ -25,17 +41,24 @@ def save_excel(b2b, see, ids):
     for val in ids:
         result_sheet.append([ val["bgm"], val["ko_count"], val["unh_unt_result"] ])
 
-    cell_range = result_sheet['C2':'C' + str(1 + len(ids))] #TODO check
+    cell_range = result_sheet['C2':'C' + str(1 + len(ids))]
     for row in cell_range:
         for cell in row:
             cell.fill = PatternFill("solid", fgColor="C8F7C8" if cell.value == "OK" else "FFCCCB")
             cell.font = Font(color="0A5D00" if cell.value == "OK" else "800000")
+
+    set_borders_style(result_sheet, 'A1', 'C' + str(1 + len(ids)))
 
     for id in ids:
         sheet = wb.create_sheet(title="ID"+id["bgm"])
 
         sheet.append(["B2B Message", "Seeburger Message", "Result", "Note",
                       "", "Segment", "Counter", "Result", "", "Numero KO"])
+        sheet.column_dimensions['A'].width = 45
+        sheet.column_dimensions['B'].width = 45
+        sheet.column_dimensions['C'].width = 7
+        sheet.column_dimensions['D'].width = 25
+        sheet.column_dimensions['J'].width = 13
 
         for b2b_see_result in id["diff"]:
             sheet.append([b2b_see_result[0], b2b_see_result[1], b2b_see_result[2]])
@@ -62,6 +85,14 @@ def save_excel(b2b, see, ids):
             for cell in row:
                 cell.font = Font(bold=True)
                 cell.fill = PatternFill("solid", fgColor="00FFCC00")
+
+        set_borders_style(sheet, 'A1', 'D' + str(1 + len(id["diff"])))
+        set_borders_style(sheet, 'F1', 'H3')
+        set_borders_style(sheet, 'J1', 'J2')
+
+        sheet['E1'].fill = PatternFill(fill_type=None)
+        sheet['I1'].fill = PatternFill(fill_type=None)
+
 
 
 #    wb.save(filename = "./diff-" + datetime.today().strftime('%Y%m%d%H%M%S') + ".xlsx")
